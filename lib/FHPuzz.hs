cf6d26e5a73628229735f83716cc733da0ed0d17
Charles Martin <177694+instinctive@users.noreply.github.com>
2025-09-25 20:27:25 -0700

    Cleanup numbers and remove showOrigin

--- lib/FHPuzz.hs
+++ lib/FHPuzz.hs
@@ -10,22 +10,22 @@ import Diagrams.Prelude
 
 boxes :: Diagram B
 boxes = position
-    [ mk green  611 82   0
-    , mk green  798 421  30
-    , mk purple 604 220  (-30)
-    , mk purple 495 551  60
-    , mk purple 825 667  52
-    , mk blue   273 397  (-69)
-    , mk blue   234 606  (-69)
-    , mk blue   439 539  (-67)
-    , mk blue   449 791  20
-    , mk yellow 637 291  (-39)
-    , mk yellow 562 866  20
-    , mk white  932 331  61
-    , mk white  783 548  61
-    , mk orange 777 692  0
-    , mk orange 585 1035 0
-    , mk orange 893 911  30
+    [ mk  green 611   82 $   0
+    , mk  green 798  421 $  30
+    , mk purple 604  220 $ -30
+    , mk purple 495  551 $  60
+    , mk purple 825  667 $  52
+    , mk   blue 273  397 $ -69
+    , mk   blue 234  606 $ -69
+    , mk   blue 439  539 $ -67
+    , mk   blue 449  791 $  20
+    , mk yellow 637  291 $ -39
+    , mk yellow 562  866 $  20
+    , mk  white 932  331 $  61
+    , mk  white 783  548 $  61
+    , mk orange 777  692 $   0
+    , mk orange 585 1035 $   0
+    , mk orange 893  911 $  30
     ]
   where
     mk color x y angle =
@@ -39,7 +39,7 @@ fhpuzzMain = do
     Right img <- loadImageEmb "full.png"
     mainWith
         (
-            boxes # showOrigin
+            boxes
         <> 
             image img
                 # sized (dims2D 1477 1218)
